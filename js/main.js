b5e80bef32f9fd51e3a9b75c74ab4f29842041b3
Moshe666 <125701392+Moshe666@users.noreply.github.com>
2023-08-12 21:31:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -94,12 +94,14 @@
             
             const accordionList = e.currentTarget
             const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
-            const accordionOpenedContent = accordionList.querySelector('.accordion-list__content')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
             
             const accordionControl = e.target.closest('.accordion-list__control')
             if (!accordionControl ) return
             const accordionItem = accordionControl.parentElement
             const accordionContent = accordionControl.nextElementSibling
+
+            
 
             if (accordionOpenedItem && accordionItem != accordionOpenedItem ) {
                 accordionOpenedItem.classList.remove('accordion-list__item--opened')
